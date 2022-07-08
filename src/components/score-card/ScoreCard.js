cd92f5444d3ae7af56f1b1332e032acfadc3d942
Shubham <25881429+shoebham@users.noreply.github.com>
2022-07-08 23:15:02 +0530

    init build

---
+++ src/components/score-card/ScoreCard.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+const Scorecard=()=>{
+    const [score,setScore] = useState(0);
+    const [bestScore,setBestScore] = useState(0);
+
+    const handleScore = ()=>{
+        setScore(score+1);
+    }
+
+    const handleBestScore = (score)=>{
+        setBestScore(score);
+    }
+
+
+
+    useEffect(()=>{
+        console.log(score);
+    }
+    ,[score]);
+
+    
+    return(
+        <div>
+            <h1>Score:{score}</h1>
+            <h1>Best Score:{bestScore}</h1>
+            <button onClick={handleScore}>+</button>
+            <button onClick={()=>handleBestScore(score)}>Best Score</button>
+        </div>
+    )
+}
+export default Scorecard;
